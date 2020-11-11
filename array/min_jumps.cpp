60e7a5735824bb793281ddbec613be267fd32450
RAMAN DWIVEDI <ramandwivedi20@gmail.com>
2020-11-11 13:29:19 +0530

    min jump problem

--- array/min_jumps.cpp
+++ array/min_jumps.cpp
@@ -2,18 +2,39 @@
 
 using namespace std;
 
-int max(int i, int j)
+int max(int x, int y)
 {
-	return (x>y)?x:y;
+	return (x > y) ? x : y;
 }
 
 int main()
 {
-	int arr[] = {1,4,3,2,6,7};
+	int arr[] = {1, 4, 3, 2, 6, 7};
 	int max_reach{arr[0]};
-	int jump{0};
+	int jump{1};
 	int step{arr[0]};
-	int i = sizeof(arr)/sizeof(arr[0]);
-	while()
+	int size = sizeof(arr) / sizeof(arr[0]);
+	for (int i = 1; i < size; i++)
+	{
+		if (i == size - 1)
+		{
+			cout << jump;
+			break;
+		}
+
+		max_reach = max(max_reach, i + arr[i]);
+		step--;
+
+		if (step == 0)
+		{
+			jump++;
+			if (i >= max_reach)
+			{
+				return -1;
+			}
+			step = max_reach - i;
+		}
+	}
+
 	return 0;
 }
