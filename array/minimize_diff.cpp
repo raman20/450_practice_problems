4778ecc68259999809f6add5cc4b9e3078e9969b
RAMAN DWIVEDI <ramandwivedi20@gmail.com>
2020-11-11 19:28:32 +0530

    minimize diff

---
+++ array/minimize_diff.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    int arr[] = {1 ,8 ,9 ,10 ,16};
+    int size = sizeof(arr)/sizeof(arr[0]);
+    int k;
+    cin >> k;
+    std::sort(arr,arr+size);
+    int ans = arr[size-1]-arr[0];
+    int max,min;
+    for(int i=0;i<size;i++)
+    {
+        max = std::max(arr[i]+k, arr[size-1] - k);
+        min = std::min(arr[0]+k, arr[i+1] - k);
+        ans = std::min(ans, max - min);
+    }
+    cout << ans;
+    return 0;
+}
