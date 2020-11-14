5f85ba0c6f7a9583f5d27e77fb26f1855d44689b
raman20 <ramandwivedi20@protonmail.com>
2020-11-14 16:47:04 +0530

    kadanes algo update

--- array/kadanes.cpp
+++ array/kadanes.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 int main()
 {
-	int arr[] = {-4,-6,1,4,-7,2,-5,-7,4};
+	int arr[] = {12,4,-4,6,-6,10,-11};
 	int max{arr[0]};
 	int sum{0};
 	//kadane algo
@@ -12,11 +12,9 @@ int main()
 	for(int i=0; i<sizeof(arr)/sizeof(arr[0]); i++)
 	{
 		sum += arr[i];
-		if(max < sum && sum > 0)
-		{
-			max = sum;
-		}
-		else
+		
+		max = std::max(max,sum);
+		if(sum < 0)
 		{
 			sum = 0;
 		}
