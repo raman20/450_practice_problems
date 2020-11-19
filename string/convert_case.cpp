49af77462fd7bd67b41cb4070be6ddfdd2d5bc30
raman20 <ramandwivedi20@protonmail.com>
2020-11-19 15:52:57 +0530

    added inbuilt functions

--- string/convert_case.cpp
+++ string/convert_case.cpp
@@ -14,14 +14,22 @@ int main()
 		{
 			s[i] -= 32;
 		}
-	}	cout << "to upper-case " << s << endl;
+	}	
+	cout << "to upper-case " << s << endl;
 
 	for(int i=0; i<s.size(); i++)
 	{
 		if(s[i] >= 'A' && s[i]<= 'Z' )
 		{
 			s[i] += 32;
-		}	}cout<< "to lower-case " << s << endl;
+		}	
+	}
+	cout<< "to lower-case " << s << endl;
+
+	transform(s.begin(),s.end(),s.begin(),::toupper);
+	cout << "transform to upper " << s << endl;
+	transform(s.begin(),s.end(),s.begin(),::tolower);
+	cout << "transform to lower " << s << endl;
 
 	return 0;
 }
