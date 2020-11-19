b4465a9bd2fe840d8bc5d5ae44beb239c84900bb
raman20 <ramandwivedi20@protonmail.com>
2020-11-19 15:47:41 +0530

    case conversion

---
+++ string/convert_case.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <algorithm>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+	string s;
+	getline(cin,s);
+	for(int i=0; i<s.size(); i++)
+	{
+		if(s[i] >= 'a' && s[i]<= 'z' )
+		{
+			s[i] -= 32;
+		}
+	}	cout << "to upper-case " << s << endl;
+
+	for(int i=0; i<s.size(); i++)
+	{
+		if(s[i] >= 'A' && s[i]<= 'Z' )
+		{
+			s[i] += 32;
+		}	}cout<< "to lower-case " << s << endl;
+
+	return 0;
+}
