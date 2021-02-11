960e04025964346652d777a656d013ebfbe2a5ec
raman20 <ramandwivedi20@protonmail.com>
2021-02-11 15:11:55 +0530

    function name change

--- string/lrs.cpp
+++ string/lrs.cpp
@@ -1,18 +1,18 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int lcs(string str1, int size1, string str2, int size2)
+int lrs(string str1, int size1, string str2, int size2)
 {
     if(size1 > 0 && size2 > 0)
     {
         if(str1[size1-1] == str2[size2-1] && (size1-1) != (size2-1))
         {
-            return 1 + lcs(str1,size1-1,str2,size2-1);
+            return 1 + lrs(str1,size1-1,str2,size2-1);
         }
         else
         {
         
-            return std::max(lcs(str1,size1-1,str2,size2),lcs(str1,size1,str2,size2-1));
+            return std::max(lrs(str1,size1-1,str2,size2),lrs(str1,size1,str2,size2-1));
         }
     }
     else
@@ -26,6 +26,6 @@ int main()
 {
     string s1;
     cin >> s1;
-    cout << lcs(s1,s1.size(),s1,s1.size());
+    cout << lrs(s1,s1.size(),s1,s1.size());
     return 0;
 }
