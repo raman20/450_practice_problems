500ac166542fb420e3f72dee9874dcfa5cdb7c7a
raman20 <ramandwivedi20@protonmail.com>
2021-06-09 21:50:02 +0530

    update

--- array/max_triplet_sum.js
+++ array/max_triplet_sum.js
@@ -3,12 +3,15 @@
 function solution(a) {
     let max = 0;
     for (let i = 0; i < a.length; i++) {
+
         let max1 = 0, max2 = 0;
+        
         for (let j = 0; j < i; j++) {
             if (a[j] < a[i]) {
                 max1 = Math.max(max1, a[j]);
             }
         }
+        
         for (let j = i + 1; j < a.length; j++) {
             if (a[j] > a[i]) {
                 max2 = Math.max(max2, a[j]);
